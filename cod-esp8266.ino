ecc8de96a37dac0bf2651f02985437c50d5fb204
gabifeliciano <116607550+gabifeliciano@users.noreply.github.com>
2024-11-08 21:51:25 -0300

    Update cod-esp8266.ino

--- cod-esp8266.ino
+++ cod-esp8266.ino
@@ -1,67 +1,49 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Definições da rede Wi-Fi
+// Informações da rede Wi-Fi
 const char* ssid = "SEU_SSID";           
-const char* password = "SUA_SENHA";      
+const char* password = "SUA_SENHA";     
 
-// Definições do MQTT
-const char* mqtt_server = "test.mosquitto.org"; 
-const char* mqtt_user = "";                      
-const char* mqtt_pass = "";                      
+// Informações do Broker MQTT
+const char* mqtt_server = "BROKER_IP";   
+const char* mqtt_user = "USUARIO_MQTT";  
+const char* mqtt_pass = "SENHA_MQTT";    
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Pinos do ESP8266
+// Pino do relé (para monitoramento)
 int relayPin = 2;  
 
-// Função de callback do MQTT (será chamada quando uma mensagem for recebida)
 void callback(char* topic, byte* payload, unsigned int length) {
-  String message = "";
-  
-  for (int i = 0; i < length; i++) {
-    message += (char)payload[i];
-  }
-
-  // Se a mensagem for "LIGAR", acende a lâmpada
-  if (message == "LIGAR") {
-    digitalWrite(relayPin, HIGH);
-  }
-  // Se a mensagem for "DESLIGAR", apaga a lâmpada
-  else if (message == "DESLIGAR") {
-    digitalWrite(relayPin, LOW);
-  }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Conecta ao Wi-Fi
+  // Conexão com Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Conectando ao Wi-Fi...");
   }
   Serial.println("Conectado ao Wi-Fi!");
-  Serial.print("Endereço IP: ");
-  Serial.println(WiFi.localIP());
 
-  // Conecta ao servidor MQTT
-  client.setServer(mqtt_server, 1883);  
+  // Configuração do MQTT
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW);  
+  pinMode(relayPin, INPUT);  // Usando o pino para monitorar o status da lâmpada
 }
 
 void reconnect() {
-  // Loop até se conectar ao MQTT
+  // Reconexão ao broker MQTT se necessário
   while (!client.connected()) {
     Serial.print("Tentando conexão MQTT...");
     if (client.connect("ESP8266Client", mqtt_user, mqtt_pass)) {
       Serial.println("Conectado ao broker MQTT!");
-      client.subscribe("lampada/status");  // Assina o tópico onde o comando será enviado
+      client.subscribe("lampada/status");
     } else {
       Serial.print("Falha ao conectar, rc=");
       Serial.print(client.state());
@@ -74,5 +56,17 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  client.loop(); // Verifica por mensagens MQTT
+  client.loop(); 
+
+  // Monitoramento do status da lâmpada
+  int lampStatus = digitalRead(relayPin);  
+
+  // Envia o status da lâmpada para o MQTT
+  if (lampStatus == HIGH) {
+    client.publish("lampada/status", "LIGADA");
+  } else {
+    client.publish("lampada/status", "DESLIGADA");
+  }
+
+  delay(1000);  // Publica o status a cada 1 segundo
 }
