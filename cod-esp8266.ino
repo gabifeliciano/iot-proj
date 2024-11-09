f4c1f47576d123648406e6b4722c84bf322f15da
gabifeliciano <116607550+gabifeliciano@users.noreply.github.com>
2024-11-08 21:13:45 -0300

    Update cod-esp8266.ino

--- cod-esp8266.ino
+++ cod-esp8266.ino
@@ -1,19 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+// Definições da rede Wi-Fi
 const char* ssid = "SEU_SSID";           
 const char* password = "SUA_SENHA";      
 
 // Definições do MQTT
-const char* mqtt_server = "test.mosquitto.org";  
+const char* mqtt_server = "test.mosquitto.org"; 
 const char* mqtt_user = "";                      
 const char* mqtt_pass = "";                      
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// Pinos do ESP8266
 int relayPin = 2;  
 
+// Função de callback do MQTT (será chamada quando uma mensagem for recebida)
 void callback(char* topic, byte* payload, unsigned int length) {
   String message = "";
   
@@ -21,9 +24,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
     message += (char)payload[i];
   }
 
+  // Se a mensagem for "LIGAR", acende a lâmpada
   if (message == "LIGAR") {
     digitalWrite(relayPin, HIGH);
   }
+  // Se a mensagem for "DESLIGAR", apaga a lâmpada
   else if (message == "DESLIGAR") {
     digitalWrite(relayPin, LOW);
   }
@@ -32,6 +37,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup() {
   Serial.begin(115200);
 
+  // Conecta ao Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -50,11 +56,12 @@ void setup() {
 }
 
 void reconnect() {
+  // Loop até se conectar ao MQTT
   while (!client.connected()) {
     Serial.print("Tentando conexão MQTT...");
     if (client.connect("ESP8266Client", mqtt_user, mqtt_pass)) {
       Serial.println("Conectado ao broker MQTT!");
-      client.subscribe("lampada/status");  
+      client.subscribe("lampada/status");  // Assina o tópico onde o comando será enviado
     } else {
       Serial.print("Falha ao conectar, rc=");
       Serial.print(client.state());
